e621acfc1fdba40e8aaee767219385b4e88d80a8
AkhilPoojary <akhilsru3@gmail.com>
2024-05-10 14:49:32 +0530

    welcome

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.time.Clock;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book");
+    }
+}
